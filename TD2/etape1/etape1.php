ca3262d38339ddd5cdc0997b66148ad44e29f0b2
Jb <59555781+MyNameIsKaiKo@users.noreply.github.com>
2020-05-25 02:20:10 +0200

    Add files via upload

---
+++ TD2/etape1/etape1.php
@@ -0,0 +1,31 @@
+<!-- Etape 1 : tester le formulaire avec php -->
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>TD2-SIN 2020</title>
+		<style type="text/css"></style>
+	</head>
+	<body>
+        <form action="" method ="POST">
+            <input type="text" name="nom" value="">
+            <input type="text" name="prenom" value="">
+            <input type="text" name="statut" value="">
+            <input type="text" name="date" value="">
+            <input type="submit" name="" value="submit">
+        </form>
+        <?php
+            // récupération des données du formulaire
+            $nom= $_POST['nom'];	
+            $prenom= $_POST['prenom'];
+            $statut= $_POST['statut'];
+            $date= $_POST['date'];
+            //afficher les données ici dans des div 
+            echo "<div> $nom </div>";
+            echo "<div> $prenom </div>";
+            echo "<div> $statut </div>";
+            echo "<div> $date </div>";
+        ?>
+    </body>
+  
+</html>
+
